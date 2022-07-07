94da773130a9ae58e16f62e5e1dca5f79c2099ef
Your Name <tpunchihewage@gmail.com>
2022-07-07 12:34:21 +0200

    modified solutions.js

---
+++ solution.js
@@ -0,0 +1,22 @@
+// Exercise - 01
+
+console.log("1.1", 2);
+
+console.log("1.2", 7);
+
+console.log("1.3", 9);
+
+// Exercise - 02
+
+let myVar = true;
+
+console.log(myVar !== true ? "good morning" : "good evening");
+
+// Exercise - 03
+
+let firstName = "" || "John" || 0;
+let emptyStr = "" && false && "Hello World";
+let zero = "" || false || 0;
+let seven = 75 && "nine" && 7;
+
+console.log(firstName, emptyStr, zero, seven);
