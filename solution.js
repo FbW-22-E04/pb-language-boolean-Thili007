60bb0afaccaf876fd1f1b8240f7b536ec3a47602
Your Name <tpunchihewage@gmail.com>
2022-07-07 12:40:34 +0200

    modified files

--- solution.js
+++ solution.js
@@ -1,10 +1,14 @@
-// Exercise - 01
+// Exercise - 01.1
 
-console.log("1.1", 2);
+console.log(3 == "3");
 
-console.log("1.2", 7);
+// Exercise - 01.2
 
-console.log("1.3", 9);
+console.log(2);
+
+console.log(7);
+
+console.log(9);
 
 // Exercise - 02
 
